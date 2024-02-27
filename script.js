a1cdcdb7393084c10d6b2a10427cf038c0d81642
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-02-27 18:10:29 +0000

    Problem #3

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-/* 
+/*
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
+
+  E para cada operação, mostrar um alerta
+  com o resultado.
+*/
+
+let firstNumber = prompt('Digite o primeiro número')
+let secondNumber = prompt('Digite o segundo número')
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const multi = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert('Soma: ' + sum)
+alert('Subtração: ' + sub)
+alert('Multiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da divisão: ' + restDiv)
+
+
+/*
   Solicite 2 números, faça a soma deles
   e apresente o resultado final ao usuário
 */
 
-alert("Iremos somar 2 números")
-let numberOne = prompt("Digite o primeiro número:")
-let numberTwo = prompt("Digite o segundo número:")
-let result = Number(numberOne) + Number(numberTwo)
-alert("Resultado final: " + result)
+// alert("Iremos somar 2 números")
+// let numberOne = prompt("Digite o primeiro número:")
+// let numberTwo = prompt("Digite o segundo número:")
+// let result = Number(numberOne) + Number(numberTwo)
+// alert("Resultado final: " + result)
 
 
 /*
