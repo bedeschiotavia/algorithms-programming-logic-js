9c2a456e76eebd17b19917958f9cfbfd3b9c806b
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-02-28 19:53:24 +0000

    Problem #8

--- 08/script.js
+++ 08/script.js
@@ -10,32 +10,32 @@
   Escreva uma lista contendo o nome dos pacientes
 */
 
-// const patients = [
-//   {
-//     name: "Luiz",
-//     age: 20,
-//     weight: 100,
-//     height: 190,
-//   },
-//   {
-//     name: "Alexandra",
-//     age: 27,
-//     weight: 70,
-//     height: 170,
-//   },
-//   {
-//     name: "Carlos",
-//     age: 42,
-//     weight: 90,
-//     height: 180,
-//   },
-// ]
+const patients = [
+  {
+    name: "Luiz",
+    age: 20,
+    weight: 100,
+    height: 190,
+  },
+  {
+    name: "Alexandra",
+    age: 27,
+    weight: 70,
+    height: 170,
+  },
+  {
+    name: "Carlos",
+    age: 42,
+    weight: 90,
+    height: 180,
+  },
+]
 
-// let patientsNames = []
+let patientsNames = []
 
-// for(let patient of patients) {
-//   patientsNames.push(patient.name)
-// }
+for(let patient of patients) {
+  patientsNames.push(patient.name)
+}
 
-// alert(patientsNames)
+alert(patientsNames)
 
